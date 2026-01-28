b12004372bcb707607bc203155cb0637f24f37b6
Shen Chen <shenchen0558@gmail.com>
2026-01-28 15:26:29 -0500

    week10

--- controllers/runs.js
+++ controllers/runs.js
@@ -1,32 +1,62 @@
-const Run = require("../models/Run")
+const Run = require("../models/run")
 const { StatusCodes} = require("http-status-codes")
 const {BadRequestError, NotFoundError} = require('../errors')
 
 const getAllRuns = async(req,res) =>{
-    res.send('get all jobs')
+    const runs = await Run.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({runs, count:runs.length})
 }
 
 const getRun = async(req,res) =>{
-    res.send('get job')
+    const {user:{userId},params:{id:runId}} = req
+
+    const run = await Run.findOne({
+            _id:runId,createdBy:userId
+    })
+
+    if(!run){
+        throw new NotFoundError(`No run with id ${runId}`)
+    }
+
+    res.status(StatusCodes.OK).json({run})
 }
 
 const createRun = async(req,res) =>{
-    try {
-        req.body.createdBy = req.user.userId 
-        const run = await Run.create(req.body)
-        res.status(StatusCodes.CREATED).json({run})
-    } catch (error) {
-         res.status(400).json({msg:error.message})
-    }
+    req.body.createdBy = req.user.userId 
+    const run = await Run.create(req.body)
+    res.status(StatusCodes.CREATED).json({run})
     
 }
 
 const updateRun = async(req,res) =>{
-    res.send('update job')
+    const {
+            body:{title, distance, duration},
+            user:{userId},
+            params:{id:runId}} = req
+
+    if(title===''||distance===''||duration===''){
+            throw new BadRequestError('Title, Distance, or Duration fields cannot be empty')
+    }
+
+    const run = await Run.findByIdAndUpdate({_id:runId, createdBy:userId}, req.body, {new:true, runValidators:true})
+
+    if(!run){
+            throw new NotFoundError(`No run with id ${runId}`)
+    }
+    res.status(StatusCodes.OK).json({run})
 }
 
 const deleteRun = async(req,res) =>{
-    res.send('delete job')
+    const {user:{userId},params:{id:runId}} = req
+
+    const run = await Run.findOneAndDelete({
+        _id:runId,
+        createdBy:userId
+    })
+    if(!run){
+        throw new NotFoundError(`No run with id ${runId}`)
+    }
+    res.status(StatusCodes.OK).send() 
 }
 
 module.exports ={
--- models/User.js
+++ models/User.js
@@ -5,20 +5,20 @@ const jwt = require("jsonwebtoken")
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:[true, 'Please porvide name'],
+        required:[true, 'Please provide name'],
         minlength:3,
         maxlength:50,
     },
     email:{
         type:String,
-        required:[true, 'Please porvide email'],
+        required:[true, 'Please provide email'],
         match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please provide valid email'],
         unique:true,
     },
     password:{
         type:String,
-        required:[true, 'Please porvide password'],
+        required:[true, 'Please provide password'],
         minlength:6,
     },
 })
